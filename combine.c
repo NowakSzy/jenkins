72c960fc83497506f99b819b1e90e8389b842031
NowakSzy <szymon.nowak2003@wp.pl>
2026-06-14 22:10:28 +0200

    Update combine.c

--- combine.c
+++ combine.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mongoose.h" // Pobierane automatycznie z drugiego Gita
+#include "mongoose.h"
 #include "logger.h"
 
 int main(int argc, char *argv[]) {
     struct mg_mgr mgr;
 
-    // Obsługa wariantu dynamicznego przez argument linii poleceń
     if (argc > 1) {
         dynamic_log_level = atoi(argv[1]);
         printf("[SYSTEM] Zmiana DYNAMICZNA poziomu logow na: %d\n", dynamic_log_level);
@@ -15,7 +14,6 @@ int main(int argc, char *argv[]) {
     LOG_INFO("MONGOOSE_SERVER", 101, "Uruchamianie serwera Mongoose...", "");
     mg_mgr_init(&mgr);
 
-    // Symulacja zdarzeń sieciowych
     LOG_WARN("MONGOOSE_SERVER", 102, "Ostrzezenie: Wykryto wolne polaczenie", "Klient_ID=45");
     LOG_ERR("MONGOOSE_SERVER", 103, "Blad krytyczny odczytu z gniazda!", "Socket_Error_Code=104");
 
